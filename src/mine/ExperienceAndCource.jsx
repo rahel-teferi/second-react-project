d4352ea8422fa23d51e2a2b52fcee9c618586f3f
Rahel Teferi <rahel.tem@gmail.com>
2025-04-04 13:30:27 +0200

    updating the first page

--- src/mine/ExperienceAndCource.jsx
+++ src/mine/ExperienceAndCource.jsx
@@ -1,65 +1,80 @@
 import { NavLink } from "react-router-dom";
-
+import { MdOutlineWork } from "react-icons/md";
+import { GiGraduateCap } from "react-icons/gi";
 export const ExperienceAndCource = () => {
   return (
     <div className="school">
-      <h3>Work Experience</h3>
+      <div className="titles">
+        <MdOutlineWork />
+        <p> Work Experience</p>
+      </div>
+      <div className="content">
+        <p>2016 - 2023</p>
 
-      <p>2016 - 2023</p>
+        <h4 style={{ textDecoration: "underline" }}>
+          NIRx Medizintechnik GmbH
+        </h4>
 
-      <h4 style={{ textDecoration: "underline" }}>NIRx Medizintechnik GmbH</h4>
+        <p style={{ paddingBottom: "10px" }}>Logistics Team lead</p>
+        <p>
+          Process, fulfill, and track customer orders using ERP system Microsoft
+          dynamics 365
+        </p>
+        <p>
+          Work closely with the sales team to handle orders and provide customer
+          service
+        </p>
 
-      <p style={{ paddingBottom: "10px" }}>Logistics Team lead</p>
-      <p>
-        Process, fulfill, and track customer orders using ERP system Microsoft
-        dynamics 365
-      </p>
-      <p>
-        Work closely with the sales team to handle orders and provide customer
-        service
-      </p>
+        <p>Preparing reports and work instructions for processes</p>
+      </div>
 
-      <p>Preparing reports and work instructions for processes</p>
-      <h3>Courses</h3>
+      <div className="titles">
+        <GiGraduateCap />
+        <p>Courses</p>
+      </div>
+      <div className="content">
+        <p>2024 - 2025</p>
 
-      <p>2024 - 2025</p>
-
-      <h4 style={{ textDecoration: "underline" }}>Hamburg Coding School</h4>
-      <p>Full-stack web development</p>
-      <ul style={{ paddingLeft: "20px", paddingTop: "10px" }}>
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>Javascript</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>React</li>
-        <li>Git</li>
-        <li>MySQL</li>
-        <li>Bootstrap</li>
-      </ul>
-      <h3>project with HTML, CSS, JS and Bootstrap</h3>
-      <a
-        href="https://rahel-teferi.github.io/hcs-project/html/index.html"
-        target="_blank"
-      >
-        <h5>Amazing Animals: Learn and Quiz!</h5>
-      </a>
-      <h3>React practice projects</h3>
-      <NavLink to="digitalClock">
-        <h5>Digital Clock</h5>
-      </NavLink>
-      <NavLink to="quoteGenerator">
-        <h5>Random quote generator</h5>
-      </NavLink>
-      <NavLink to="carosel">
-        <h5>Carosel</h5>
-      </NavLink>
-      <NavLink to="shoppingList">
-        <h5>Shopping list</h5>
-      </NavLink>
-      <NavLink to="searchUser">
-        <h5>Search User</h5>
-      </NavLink>
+        <h4 style={{ textDecoration: "underline" }}>Hamburg Coding School</h4>
+        <p>Full-stack web development</p>
+        <ul style={{ paddingLeft: "20px", paddingTop: "10px" }}>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>Javascript</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>React</li>
+          <li>Git</li>
+          <li>MySQL</li>
+          <li>Bootstrap</li>
+        </ul>
+      </div>
+      <div className="titles">project with HTML, CSS, JS and Bootstrap</div>
+      <div className="content">
+        {" "}
+        <a
+          href="https://rahel-teferi.github.io/hcs-project/html/index.html"
+          target="_blank"
+        >
+          <h5>Amazing Animals: Learn and Quiz!</h5>
+        </a>
+        <h3>React practice projects</h3>
+        <NavLink to="digitalClock">
+          <h5>Digital Clock</h5>
+        </NavLink>
+        <NavLink to="quoteGenerator">
+          <h5>Random quote generator</h5>
+        </NavLink>
+        <NavLink to="carosel">
+          <h5>Carosel</h5>
+        </NavLink>
+        <NavLink to="shoppingList">
+          <h5>Shopping list</h5>
+        </NavLink>
+        <NavLink to="searchUser">
+          <h5>Search User</h5>
+        </NavLink>
+      </div>
     </div>
   );
 };
--- src/mine/ProfilePic.jsx
+++ src/mine/ProfilePic.jsx
@@ -6,10 +6,8 @@ export const ProfilePic = () => {
     <div className="profile-pic">
       <img src={pic} />
       <h2>Rahel Teferi</h2>
+      <p>Junior full-stack developer</p>
       <div className="contact">
-        <h4>About Me</h4>
-        <p>Full-stack developer</p>
-
         <h4>Contact</h4>
         <p>
           Email: <a href="mailto:'rahel.tem@gmail.com'">rahel.tem@gmail.com</a>
